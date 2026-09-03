4f57d9bbb3760db851d055594ad051cad8f36441
chtang <chtang@hmc.edu>
2026-09-02 19:49:26 -0700

    updated loops

--- util/string.c
+++ util/string.c
@@ -4,14 +4,16 @@
 
 /*
  * @brief Returns length of string
- * 
+ *
  * @param[in] *str The input string
- * @param[out] strlen The length of the string 
+ * @param[out] strlen The length of the string
  */
-long strlen(const char *str) {
+long strlen(const char *str)
+{
     long len = 0;
 
-    while (*(str+len) != '\0') {
+    while (*(str + len) != '\0')
+    {
         len++;
     }
 
@@ -20,27 +22,35 @@ long strlen(const char *str) {
 
 /*
  * @brief Returns a pointer to the first index of a substring within another string
- * 
+ *
  * @param[in] *haystack The given string
  * @param[in] *needle The specified substring
  * @param[out] *strstr The pointer to the first index of the substring in the string
  */
-char *strstr(const char *haystack, const char *needle) {
+char *strstr(const char *haystack, const char *needle)
+{
     // Check if substring is longer than string
     long needle_len = strlen(needle);
     long haystack_len = strlen(haystack);
-    if (needle_len > haystack_len) {
+    if (needle_len > haystack_len)
+    {
         return NULL;
     }
-    for (long i = 0; i <= haystack_len - needle_len; ++i) {
-        for (long j = 0; j < needle_len; ++j) {
-            if (*(needle+j) != *(haystack+i+j)) {
+    for (long i = 0; i <= haystack_len - needle_len; ++i)
+    {
+        long j = 0;
+        while (j < needle_len)
+        {
+            if (*(needle + j) != *(haystack + i + j))
+            {
                 break;
             }
+            ++j;
         }
         // Check if the whole substring was found in the string
-        if (j == needle_len) {
-            return haystack+i;
+        if (j == needle_len)
+        {
+            return haystack + i;
         }
     }
     return NULL;
@@ -53,16 +63,21 @@ char *strstr(const char *haystack, const char *needle) {
  * @param[in] *src The source string.
  * @param[in] n The number of characters to copy.
  */
-char *strncat(char *dest, const char *src, long n) {
+char *strncat(char *dest, const char *src, long n)
+{
     // compare length of src to n
     long srclen = strlen(src);
-    if (n > srclen) {
+    if (n > srclen)
+    {
         n = srclen;
     }
 
     long destlen = strlen(dest);
-    for (long i = 0; i < n; ++i) {
+    long i = 0;
+    while (i < n)
+    {
         *(dest + destlen + i) = *(src + i);
+        ++i;
     }
     *(dest + destlen + i) = '\0';
     return dest;
@@ -70,32 +85,36 @@ char *strncat(char *dest, const char *src, long n) {
 
 /*
  * @brief Compares the first n characters of two string; returns an integer indicating which one is greater
- * 
+ *
  * @param[in] *s1 The first input string
  * @param[in] *s2 The second input string
  * @param[in] n  The max number of characters to check
  * @param[out] strncmp Integer value which is positive if first string is greater, negative if second string is greater, and 0 if the two strings are equal
  */
-int strncmp(const char *s1, const char *s2, long n) {
+int strncmp(const char *s1, const char *s2, long n)
+{
     // Check string lengths to avoid long while loops due to large n
     long strlen1 = strlen(s1);
     long strlen2 = strlen(s2);
-    if (strlen1 < n) {
+    if (strlen1 < n)
+    {
         n = strlen1;
     }
-    if (strlen2 < n) {
+    if (strlen2 < n)
+    {
         n = strlen2;
     }
-    
+
     int cmp = 0;
     long i = 0;
-    while (i < n) {
-        cmp = *(s1+i) - *(s2+i);
-        if (cmp != 0) {
+    while (i < n)
+    {
+        cmp = *(s1 + i) - *(s2 + i);
+        if (cmp != 0)
+        {
             return cmp;
         }
         i++;
     }
     return 0;
-
 }
