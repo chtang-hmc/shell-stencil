6aec938a8faa5240a24d7c73d92e9a9a472e5e81
liviaordonez <liviaordonez5@gmail.com>
2026-09-05 11:45:58 -0700

    Parsing User Input

--- src/input.c
+++ src/input.c
@@ -6,13 +6,14 @@
 #include <stdlib.h> // for malloc, realloc, free only!
 
 /*
- * TODO: implement me!
+ * @brief Initialize string parsing attributes for an input
  *
  * Hint: what should the initial values of each of the attributes be input
  *       attributes be?
  */
 void userinput_init() {
-
+    input_length = 0; 
+    num_tokens = 0;
 }
 
 /*
@@ -21,7 +22,13 @@ void userinput_init() {
  * Hint: is there anything else that can be reused here?
  */
 void userinput_reset() {
+    userinput_init();
 
+    // free tokens
+    for (long i = 0; i < num_tokens; ++i) {
+        free(tokens[i]);
+    }
+    free(tokens);
 }
 
 /*
@@ -31,7 +38,7 @@ void userinput_reset() {
  *       freed!
  */
 void userinput_cleanup() {
-
+    userinput_reset();
 }
 
 /*
@@ -42,8 +49,53 @@ void userinput_cleanup() {
  *   - command should end with '\0'
  *   - don't forget about the error cases! what behaviors should be undefined?
  */
+ // TODO: handle errors and return -1
 long handle_user_input(const char *user_input, long strlen, char **command) {
-    return 0;
+    // identify beginning whitespaces
+    long i = 0;
+    while (*(user_input + i) == ' ') {
+        ++i;
+    }
+
+    // allocate new string
+    char *newstring = malloc(strlen * sizeof(char));
+    long newstring_length = 0;
+
+    // go through the user input
+    long num_whitespaces = 0;
+    while (*(user_input + i) != '\n' || i < strlen) {
+        // filter out extra whitespaces
+        if (*(user_input + i)  != ' ') {num_whitespaces = 0;}
+        else {++num_whitespaces;}
+
+        if (num_whitespaces <= 1) {
+            // add to buffer
+            newstring[newstring_length] = *(user_input + i);
+            newstring_length++;
+        }
+
+        i++;
+    }
+
+    // handle trailing white space
+    if (*(user_input + i) == '\n') {
+        if (num_whitespaces != 0) { // there was a whitespace before \n
+            newstring[newstring_length - 1] = '\0';
+        } else {
+            newstring[newstring_length] = '\0';
+        }
+    }
+
+    newstring = (char *) realloc(newstring, sizeof(char) * (newstring_length + 1));
+
+    // copy newstring to command
+    for (i = 0; i <= newstring_length; ++i) {
+        *(*command+i) = newstring[i];
+    }
+
+    // return!
+    free(newstring);
+    return newstring_length;
 }
 
 /*
@@ -60,7 +112,38 @@ long handle_user_input(const char *user_input, long strlen, char **command) {
  *     character!
  */
 long tokenize_input(char *str, long strlen, char ***tokens) {
-    return 0;
+    char **tokens_array = malloc(sizeof(char *) * something);
+
+    long i = 0;
+    long current_token_len = 0;
+
+    while (i < strlen) {
+        // find word length
+        while (*(str + i + current_token_len) != ' ' && *(str + i + current_token_len) != '\n') {
+            current_token_len++;
+        }
+
+        // create token for word
+        char *token = malloc(sizeof(char) * current_token_len);
+        for (long j = 0; j < current_token_len; ++j) {
+            *(token + j) = *(str + i + j);
+        }
+        tokens_array[num_tokens] = token;
+        num_tokens++;
+
+        // handle end of command
+        if (*(str + i + current_token_len) == '\n') {
+            token = malloc(sizeof(NULL));
+            tokens_array[num_tokens] = token;
+            num_tokens++;
+            tokens = &tokens_array; // return the allocated tokens array
+            return num_tokens;
+        }
+
+        i += current_token_len + 1;
+    }
+
+    return -1;
 }
 
 
